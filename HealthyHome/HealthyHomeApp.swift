f75d85edbbc3940f591ba746a76bf0e7d0346bd6
PeaceErik <fva89@outlook.dk>
2025-05-26 11:00:13 +0200

    Initial Commit

---
+++ HealthyHome/HealthyHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthyHomeApp.swift
+//  HealthyHome
+//
+//  Created by Fred on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HealthyHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
